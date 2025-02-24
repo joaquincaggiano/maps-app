ba4f89cdb4da7e846367a1a7d2c21a86ae226565
Joaquin Serra caggiano <joaquinserracaggiano@MacBook-Pro-de-Joaquin-2.local>
2025-02-24 22:11:16 +0100

    instalacion de react native maps

--- app/map/index.tsx
+++ app/map/index.tsx
@@ -1,12 +1,21 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-
+import { View } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 const MapScreen = () => {
   return (
-    <View>
-      <Text>MapScreen</Text>
+    <View className="flex-1 justify-center items-center">
+      <MapView
+        style={{ width: "100%", height: "100%" }}
+        // showsPointsOfInterest={false}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default MapScreen
+export default MapScreen;
